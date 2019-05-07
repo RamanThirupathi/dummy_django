841980959de9231e7a8055a9db6c0249578e2ff6
Ramanjaneya R Thirupathi <50174764+RamanThirupathi@users.noreply.github.com>
2019-05-07 11:25:25 -0400

    settings

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+storageexplorer://v=1&accountid=%2Fsubscriptions%2Fa8211dd7-7f86-47b9-8848-4976b65e0847%2FresourceGroups%2Fappsvc_rg_linux_centralus%2Fproviders%2FMicrosoft.Storage%2FstorageAccounts%2Fpythonfilestoragedjango&subscriptionid=a8211dd7-7f86-47b9-8848-4976b65e0847&resourcetype=Azure.BlobContainer&resourcename=django
