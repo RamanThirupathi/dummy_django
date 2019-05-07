3e6f3b5e8dd5377f13bf909c37b5cb79a19c5dd7
Ramanjaneya R Thirupathi <50174764+RamanThirupathi@users.noreply.github.com>
2019-05-07 11:52:09 -0400

    add hello app

--- dummy_django/urls.py
+++ dummy_django/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path(r'hello/', include('hello.urls', namespace='hello')),
+    path(r'hello/', include('hello.urls', namespace='hello')),
 ]
